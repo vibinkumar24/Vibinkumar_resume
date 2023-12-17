7abcf9748075be2474f9d19f91f71afe58e70065
vibinkumar24 <vibinpvi2001@gmail.com>
2023-12-17 20:41:55 +0530

    add Vibinkumar_resume master

---
+++ src/resume.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import './resume.css'
+
+function Resume() {
+    
+    return(
+        <>
+         <div id="firstdiv">
+        <h1 id="myname"> VIBINKUMAR V </h1>
+    </div>
+    <div class="resume-section">
+        <img id="myimg" src='https://media.licdn.com/dms/image/D5603AQHZSo463Oh7eQ/profile-displayphoto-shrink_800_800/0/1666935118421?e=2147483647&v=beta&t=kCaj3e3CX0L8TvACzgn5sT58ezumAcVj_vCMwAXsROo'/>
+        <h2>OBJECTIVE</h2>
+        <p>To pursue a challenging career and be a part of progressive
+            organization that gives a scope to enhance my knowledge and to
+            utilise my skills towards the growth of a organization.</p>
+        <h2>EDUCATION</h2>
+        <h2 class="edu-inst">Velammal Engineering College</h2>
+        <h4>2023</h4>
+        <p>B.E(EEE)</p>
+        <p id="be-perc">7.92(CGPA)</p>
+        <h2 class="edu-inst">SET Vidhiyadevi matriculation Higher Secondary School</h2>
+        <h4>2019</h4>
+        <p>HSC(Stateboard)</p>
+        <p id="hsc-perc">57.66%</p>
+        <h2 class="edu-inst">SET Vidhiyadevi matriculation Higher Secondary School</h2>
+        <h4>2017</h4>
+        <p>SSLC(Stateboard)</p>
+        <p id="sslc-perc">90.20%</p>
+        <h2>PROJECT</h2>
+        <p><b>SEA-BORDER ALERT SYSTEM USING RSSI</b></p>
+        <p> To develop a location based border identification system with
+            time using Received Signal Strength Indication (RSSI) which
+            works in all climatic conditions which can be fixed in boats.
+        </p>
+        <h2>SUBJECT OF INTEREST</h2>
+        <p>Embedded Systems</p>
+        <h2>INTERNSHIP</h2>
+        <p><b>IOT Internship</b></p>
+        <p>Internship on Internet of things completed at VERZEO
+            from 01/05/2021 to 30/06/2021.</p>
+        <p><b>EQUAD Internship</b></p>
+        <p>Completed the Internship at EQUAD Engineering Services
+            Pvt.Ltd. from 13/12/2021 to 31/12/2021.</p>
+        <h2>CO CURRICULAR ACTIVITIES</h2>
+        <p><b>PROXPO 2k21</b></p>
+        <p>Participated in a PROXPO 2k21 conducted on 30/07/21
+            by Velammal Engineering College.</p>
+        <p><b>SURGE 2k23</b></p>
+        <p>I am coordinator for an melomaniac event for our college
+            symposium ( SURGE 2K23).Held on february 2023.</p>
+        <h2>EXTRA CURRICULAR ACTIVITIES</h2>
+        <p>Won Frist Prize in District Level Handball Competition conducted
+            by School Education Department on Republic Day on
+            26/01/2016.</p>
+        <h2>CLUB MEMBERSHIP</h2>
+        <p>
+            <li>VEC - ENSAV Club - Member</li>
+        </p>
+        <p>
+            <li>Institute On Engineering India - Student Member</li>
+        </p>
+        <h2>PERSONALITY TRAITS</h2>
+        <li>Positive Attitude</li>
+        <li>Smart Worker</li>
+        <li>Team Work</li>
+        <h2>DECLARATION</h2>
+        <p>I hereby assure that all the above given information is true to my
+            knowledge and belief.</p>
+        <p>Date :</p>
+        <p>Place :</p>
+        <p id="myname2">
+            VIBINKUMAR V
+        </p>
+    </div>
+    <div id="seconddiv">
+        <h2>
+            <p>CONTACT</p>
+        </h2>
+        <p>vibinpvi2001@gmail.com</p>
+        <p>+919360249708</p>
+        <p>NO: 384, Nadakadu, Kuruvikkarambai(po),Peravurani(tk),<br/>Thanjavur(dt)-614802.</p>
+        <h2>
+            <p>TECHNICAL SKILLS</p>
+        </h2>
+        <p>HTML5</p>
+        <p>CSS</p>
+        <p>Bootstrap v5.0</p>
+        <p>JavaScript</p>
+        <p>React JS</p>
+        <p>Software testing</p>
+        <h2>
+            <p>LANGUAGES</p>
+        </h2>
+        <p>Tamil</p>
+        <p>English</p>
+        <h2>
+            <p>PERSONAL DETAILS</p>
+        </h2>
+        <p>Date of Birth : 24/06/2001</p>
+        <p>Nationality : India</p>
+        <p>Father Name : VAITHILINGAM K</p>
+    </div>
+        </>
+    )
+}
+
+export default Resume;
